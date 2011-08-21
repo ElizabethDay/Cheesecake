fc50c0abeb1e3441da6bd1ef6aec0dfad5b00ece
Aaron Day <daya-github@crazydays.org>
2011-08-21 15:19:48 -0400

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheesecake::Application.config.secret_token = 'fc6ef168adf77ed44fb5e27186f2f936df6976105d87d3c685d5f9be4b6316d0af147d9bcafc0bf700e8e23e646fe5cb837311a3f2a7cbc6ab82fd48dc3cf6ca'
