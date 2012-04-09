af679b83ecf3a8e13c19352f87b3b7967470346b
Elizabeth Day <beth-github@crazydays.org>
2012-04-09 11:57:36 -0400

    generate rspec dirs

--- test/unit/ingredient_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class IngredientTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "load chicken" do
-    chicken  = Ingredient.find_by_name "Chicken Thighs" 
-    assert_not_nil chicken
-  end
-end
