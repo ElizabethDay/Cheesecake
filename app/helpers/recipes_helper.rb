b438e4733175ad10ec9a475f69b2ec3983a9a856
Elizabeth Day <beth-github@crazydays.org>
2012-04-11 11:39:17 -0400

    add link to recipes from main page

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,6 @@
 module RecipesHelper
   #return the root address of the link
   def site_location( recipe )
-    link = 'test.com'
     /(?:http:\/\/)?(?:www\.)?([^\/]+)/.match(recipe.link)[1]
   end
 end
